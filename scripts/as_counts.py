ae9ea0939ceadf3a7aabd69f05ff3e8630656bb8
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2020-12-06 20:03:49 -0800

    finish a working version of the as_counts script

--- scripts/as_counts.py
+++ scripts/as_counts.py
@@ -3,6 +3,7 @@ import sys
 import argparse
 import pandas as pd
 from pysam import VariantFile
+from types import SimpleNamespace
 
 
 
@@ -10,6 +11,8 @@ def get_hets(vcf, sample):
     """extract heterozygous STRs and SNPs from the vcf"""
     vcf = VariantFile(vcf)
     vcf.subset_samples([sample])
+    # iterate through each variant record in the VCF
+    # TODO: make sure you're considering cases where the POS is duplicated
     for rec in vcf.fetch():
         variant = rec.samples[sample]
         is_het = variant['GT'][0] != variant['GT'][1]
@@ -20,65 +23,110 @@ def get_hets(vcf, sample):
             yield rec
 
 
-def get_str_snp_pairs(variants):
+def find_nearest(strs, snps):
+    """find the nearest of the SNPs to each STR"""
+    for STR in strs:
+        # how far is this STR from the last SNP and the current SNP?
+        # distance is infinite if they live on different chromosomes
+        distances = tuple(
+            (
+                abs(STR.pos-var[0].pos)
+                if var[0].chrom == STR.chrom else float('inf')
+            ) for var in snps
+        )
+        distance = min(distances)
+        yield STR, snps[distances.index(distance)], distance
+
+
+def get_str_snp_pairs(variants, snp_counts):
     """return tuples of (str, nearest snp)"""
-    last_snp = None
-    # iterate through the SNPs
-    while True:
-        strs = []
-        # iterate through the strs
-        for variant in variants:
-            if not variant.id.startswith("STR_"):
-                break
-            strs.append(variant)
-        # check is the final variant an STR?
-        # if it is the last het variant in the VCF, it might be an STR
+    # holds the most recent SNP
+    snp = (SimpleNamespace(chrom='', pos=float('inf')), snp_counts[:0])
+    distance = float('inf')
+    # holds the current STRs
+    strs = []
+    # iterate through the SNPs and STRs
+    for variant in variants:
+        # if we found an STR, save it for later
         if variant.id.startswith("STR_"):
-            for STR in strs:
-                yield (STR, last_snp)
-            return
-        # check: did we see any strs?
-        if len(strs):
-            # Now, we must find the nearest SNP
-            # If we didn't previously see a SNP,
-            # this is just going to be the current variant
-            if last_snp is None:
-                for STR in strs:
-                    yield (STR, variant)
-            else:
-                # we must decide whether to use the current SNP or the last one
-                snps = (last_snp, variant)
-                for STR in strs:
-                    # TODO: also consider CHROM, not just POS
-                    distances = (STR.pos-last_snp.pos, variant.pos-STR.pos)
-                    yield (STR, snps[distances.index(min(distances))])
-        else:
-            # just save the current SNP and move on
-            last_snp = variant
+            strs.append(variant)
+            continue
+        # if we're here, it means this variant is a SNP, not an STR
+        # first, we must check whether this SNP appears in the snp_counts file
+        try:
+            snp_count = snp_counts.loc[(variant.chrom, variant.pos)]
+        except KeyError:
+            continue
+        # return pairs of each STR and the nearest SNP
+        yield from find_nearest(strs, (snp, (variant, snp_count)))
+        strs = []
+        # save the current SNP and move on
+        snp = variant, snp_count
+    # yield any STRs we might have accumulated before reaching the end of the VCF
+    if distance != float('inf'):
+        yield from find_nearest(strs, (snp,))
 
 
 def get_snp_counts(snp_counts):
     """parse the SNP ASE table"""
-    return pd.read_csv(snp_counts, sep="\t", header=0, index_col=['CHR', 'POS'])
+    columns = [
+        'CHR', 'POS', 'SAMPLE_ID', 'SUBJECT_ID', 'TISSUE_ID', 'REF_ALLELE',
+        'ALT_ALLELE', 'REF_COUNT', 'ALT_COUNT', 'TOTAL_COUNT', 'REF_RATIO',
+        'OTHER_ALLELE_COUNT', 'BINOM_P', 'BINOM_P_ADJUSTED', 'GENOTYPE',
+        'VARIANT_ANNOTATION', 'GENE_ID'
+    ]
+    # rename the columns
+    # the uppercase is frankly a bit annoying
+    new_col_names = {
+        col: (col[:-len("_ID")] if col.endswith('_ID') else col).lower()
+        for col in columns
+    }
+    new_col_names['GENOTYPE'] = 'snp_gt'
+    new_col_names['REF_ALLELE'] = 'snp_ref'
+    new_col_names['ALT_ALLELE'] = 'snp_alt'
+    # use pandas to get the datatypes of each column
+    types_dict = pd.read_csv(
+        snp_counts, sep="\t", header=0, nrows=5, usecols=columns
+    ).dtypes
+    # set the 'CHR' datatype to np.dtype('O') (aka object)
+    types_dict['CHR'] = types_dict['TISSUE_ID']
+    # read the entire tsv into memory
+    # TODO: read on a stream and convert this function into a generator
+    counts = pd.read_csv(
+        snp_counts, sep="\t", header=0, dtype=dict(types_dict),
+        usecols=columns
+    )
+    return counts.rename(new_col_names, axis=1).set_index(['chr', 'pos', 'tissue'])
 
 
-def get_genes(genes):
-    """parse the gene annotations file"""
-    pass
-
-
-def get_str_counts(pairs, snp_counts, genes, sample):
+def get_str_counts(pairs, sample):
+    """extract the desired information from every STR-SNP pair"""
     for pair in pairs:
-        STR, snp = pair
-        if (snp.chrom, snp.pos) in snp_counts.index:
-            yield (STR, snp, snp_counts[(snp.chrom, snp.pos)])
-        # discard the current STR
-        # TODO: try to find a new SNP?
+        STR, (snp, snp_count), distance = pair
+        str_count = snp_count.copy()
+        # extract the columns we need:
+        # STR ID, gene ID, sample name, the ASE, the location, the STR alleles, the SNP
+        # alleles, any other SNP ASE stats, the SNP/STR distance, and the tissue ID
+        str_count.insert(0, 'str', STR.id)
+        str_count.insert(1, 'snp', snp.id)
+        str_count.insert(2, 'chrom', snp.chrom)
+        str_count.insert(3, 'str_pos', STR.pos)
+        str_count.insert(4, 'snp_pos', snp.pos)
+        STR = STR.samples[sample]
+        str_count.insert(5, 'str_ref', STR.alleles[0])
+        str_count.insert(6, 'str_alt', STR.alleles[1])
+        str_count.insert(7, 'distance', distance)
+        str_count.insert(8, 'str_gt', "{}|{}".format(*STR['GT']))
+        str_count['snp_gt'] = str_count['snp_gt'].str[len('GT;'):]
+        yield str_count
 
 
 def write_out(out, sample, str_counts):
-    print("ahhh this should be a TSV with appropriately named columns", file=out)
-    # STR, gene, sample, ASE_SNP,_location, STR a1|a2, SNP a1|a2, SNP ASE stats
+    """write each pd dataframe to the output in append mode"""
+    first_line = True
+    for str_count in str_counts:
+        str_count.to_csv(out, mode='a', sep="\t", header=first_line)
+        first_line = False
 
 
 def get_args():
@@ -88,22 +136,19 @@ def get_args():
         "by using counts from nearby SNPs."
     )
     parser.add_argument(
-        '-o', '--out', help='output file name (in tsv format)',
-        type=argparse.FileType('w')
+        '-o', '--out', help='path to TSV output (default: stdout)',
+        type=argparse.FileType('w'), default=sys.stdout
     )
     parser.add_argument(
-        'vcf', help="sorted and phased STRs (prefixed with 'STR_') and SNPs"
-    )
-    parser.add_argument(
-        'counts', help='WASP-corrected counts of SNPs for this sample'
-        # TODO: describe the structure of this tsv file better
-    )
-    parser.add_argument(
-        'genes', help='GENCODE gene annotations'
+        'counts', help='WASP-corrected counts of SNPs for this GTEX sample'
         # TODO: describe the structure of this tsv file better
     )
     parser.add_argument(
         'sample', help='the name of the sample for which to extract STR counts'
+    )
+    parser.add_argument(
+        'vcf', default="-", nargs='?',
+        help="sorted/phased VCF w/ STRs (prefix ID: 'STR_') and SNPs (default: stdin)"
     )
     args = parser.parse_args()
     return args
@@ -113,9 +158,10 @@ def main(args):
     write_out(
         args.out, args.sample,
         get_str_counts(
-            get_str_snp_pairs(get_hets(args.vcf, args.sample)),
-            get_snp_counts(args.counts),
-            get_genes(args.genes),
+            get_str_snp_pairs(
+                get_hets(args.vcf, args.sample),
+                get_snp_counts(args.counts)
+            ),
             args.sample
         )
     )
