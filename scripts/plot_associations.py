1131662b1c193ad3752b71e991b4dc44b2611664
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2020-12-08 20:45:27 -0800

    make the plots prettier

--- scripts/plot_associations.py
+++ scripts/plot_associations.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 
 
-def get_ase(ase_fname, n=None):
+def get_ase(ase_fname):
     """parse the ASE TSV for the top n most promising str-tissue pairs"""
     ase = pd.read_csv(
         ase_fname, sep="\t", header=0, index_col=['str', 'tissue'], usecols=[
@@ -18,46 +18,43 @@ def get_ase(ase_fname, n=None):
             'variant_annotation', 'gene', 'ase', 'allelic_ratio'
         ]
     )
-    str_tissue_pairs = ase.index.unique()
-    if n is None:
-        n = len(str_tissue_pairs)
-    # get the top n most promising str-tissue pairs
-    ase = ase.loc[str_tissue_pairs[:n],:]
     ase['repeat_ratio'] = ase['str_a1'].str.len()/ase['str_a2'].str.len()
     return ase
 
 
-def plot(ase, out):
+def plot(ase, out, n=None):
     """make plots for each str-tissue pair in the ase table"""
-    for STR in ase.index.unique():
+    str_tissue_pairs = ase.index.unique()
+    if n is None:
+        n = len(str_tissue_pairs)
+    for STR in str_tissue_pairs:
+        if n <= 0:
+            break
         plt.figure()
-        repeat = ase['repeat_ratio'][STR].to_numpy()
-        allelic = ase['allelic_ratio'][STR].to_numpy()
+        vals = ase[['repeat_ratio','allelic_ratio']].loc[STR].to_numpy()
+        #'ase': ase[['repeat_ratio','ase']].loc[STR].to_numpy()
+        x = vals[:,0]
+        y = vals[:,1]
         # perform a simple linear regression
-        p = np.polyfit(repeat, allelic, 1)
+        p = np.polyfit(x, y, 1)
         r_squared = 1 - (
-            sum(
-                (allelic - (p[0] * repeat + p[1]))**2
-            ) / (
-                (len(allelic) - 1) * np.var(allelic, ddof=1)
-            )
+            sum((y - (p[0]*x+p[1]))**2) / ((len(y) - 1) * np.var(y, ddof=1))
         )
         p = np.poly1d(p)
         # plot the points and the line
-        plt.scatter(repeat, allelic, color='r', label="_nolegend_")
-        plt.xlabel("Ratio of STR Lengths")
-        plt.ylabel("Allelic Expression Ratio")
+        plt.scatter(x, y, color='r', label="_nolegend_")
         plt.plot(
-            repeat,
-            p(repeat),
-            label=str(p)+"\nr-squared: "+str(round(r_squared, 2))
+            x, p(x),
+            label="{}\nr-squared: {}\nn = {}".format(p, round(r_squared, 2), len(x))
         )
         # TODO: include information about distance, tissue, gene?, variant_annotation?
+        plt.xlabel("Ratio of STR Lengths")
+        plt.ylabel("Allelic Expression Ratio")
         plt.legend(frameon=False, loc='lower right')
         plt.tight_layout()
         plt.savefig(str(out)+"/"+"-".join(STR)+".png", bbox_inches='tight')
         plt.clf()
-
+        n -= 1
 
 
 def get_args():
@@ -89,7 +86,7 @@ def get_args():
 def main(args):
     # create the output directory if it doesn't exist yet
     args.out.mkdir(exist_ok=True)
-    plot(get_ase(args.ase, args.num_plots), args.out)
+    plot(get_ase(args.ase), args.out, args.num_plots)
 
 
 if __name__ == "__main__":
